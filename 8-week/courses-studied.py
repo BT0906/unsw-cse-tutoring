e6287a88e16713a3f1542ba804d69c408dd2c324
Brandon Tan <brandon96tan@gmail.com>
2025-07-22 16:45:48 +1000

    courses studied qn start

---
+++ 8-week/courses-studied.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import sys
+import psycopg2
+
+### Constants
+USAGE = f"Usage: {sys.argv[0]} studentID term"
+
+def main(db):
+    ### Command-line args
+    if len(sys.argv) != 3:
+        print(USAGE)
+        return 1
+
+    student_id = sys.argv[1]
+    term_code = sys.argv[2]
+
+    cur = db.cursor()
+
+    
+
+    cur.close()
+    return 0
+
+if __name__ == '__main__':
+    exit_code = 0
+    db = None
+    try:
+        db = psycopg2.connect(dbname="uni")
+        exit_code = main(db)
+    except psycopg2.Error as err:
+        print("DB error:", err)
+        exit_code = 1
+    except Exception as err:
+        print("Internal Error:", err)
+        raise err
+    finally:
+        if db is not None:
+            db.close()
+    sys.exit(exit_code)
